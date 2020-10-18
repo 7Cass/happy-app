f1bd0b84a192eded61d9cb12fce5e1020bb18871
Cass <jaaopbr@gmail.com>
2020-10-18 18:25:29 -0300

    test

--- src/database/test.js
+++ src/database/test.js
@@ -3,29 +3,29 @@ const saveOrphanage = require("./saveOrphanage");
 
 Database.then(async (db) => {
   // Insert data in table
-  await saveOrphanage(db, {
-    lat: "-27.222834",
-    lng: "-49.6543839",
-    name: "Lar dos meninos",
-    about:
-      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-    whatsapp: "99898989112",
-    images: [
-      "https://images.unsplash.com/photo-1595803330237-83a3a8698450?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-      "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-    ].toString(),
-    instructions:
-      "Venha se sentir a vontade e traga muito amor e paciência para dar.",
-    opening_hours: "Horário de visitas Das 8h até 18h",
-    open_on_weekends: "0",
-  });
-  // Consult data in table
-  const selectedOrphanages = await db.all("SELECT * FROM orphanages");
-  console.log(selectedOrphanages);
+  // await saveOrphanage(db, {
+  //   lat: "-27.222834",
+  //   lng: "-49.6543839",
+  //   name: "Lar dos meninos",
+  //   about:
+  //     "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+  //   whatsapp: "99898989112",
+  //   images: [
+  //     "https://images.unsplash.com/photo-1595803330237-83a3a8698450?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+  //     "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+  //   ].toString(),
+  //   instructions:
+  //     "Venha se sentir a vontade e traga muito amor e paciência para dar.",
+  //   opening_hours: "Horário de visitas Das 8h até 18h",
+  //   open_on_weekends: "0",
+  // });
+  // // Consult data in table
+  // const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+  // console.log(selectedOrphanages);
 
   // Consult only 1 orphanage from id
-  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"');
-  console.log(orphanage);
+  // const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"');
+  // console.log(orphanage);
 
   //   // Delete data from table
   //   console.log(await db.run('DELETE FROM orphanages WHERE id = "4"'));
